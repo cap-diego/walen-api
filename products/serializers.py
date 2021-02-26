b9a638805d7ce0281e8f0e0a83b418962634c756
Diego <diegobuceta35@gmail.com>
2021-02-26 12:52:42 -0300

    Agrego a list/get de Product el campo tags

--- products/serializers.py
+++ products/serializers.py
@@ -5,9 +5,14 @@ from rest_framework import routers, serializers, viewsets
 # From w
 from products.models import Product, Category
 
+class TagListingField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.tag
 
 class ProductSerializer(serializers.ModelSerializer):
     measure_unit = serializers.CharField(source='get_measure_unit_display')
+    tags = TagListingField(many=True, read_only=True)
+
     class Meta:
         model = Product
         fields = '__all__'
