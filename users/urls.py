f2703fe4a743aedf6d050f4ec4585bf32760c1c1
Diego <diegobuceta35@gmail.com>
2021-02-27 12:40:18 -0300

    fix permission

--- users/urls.py
+++ users/urls.py
@@ -1,18 +1,10 @@
 # From django
 from django.urls import path
 
-# From drf
-from rest_framework.routers import DefaultRouter
-
 # From w 
-from users.views import UserViewSet, get_user
-
-
-router = DefaultRouter()
-router.register(r'', UserViewSet, basename='user')
+from users.views import get_user, new
 
 urlpatterns = [
     path('<str:email>', get_user),
+    path('new/', new)
 ]
-
-urlpatterns += router.urls
--- users/views.py
+++ users/views.py
@@ -1,13 +1,12 @@
 # From django
-from django.http import HttpResponse,  HttpResponseNotFound, HttpResponseBadRequest
 from django.contrib.auth import get_user_model
-from django.views.decorators.http import require_http_methods
 
 # From drf
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view, \
+    permission_classes
 from rest_framework import status
 
 # utils 
@@ -20,42 +19,38 @@ from users.auth import magiclink
 User = get_user_model()
 APPLICATION_JSON = 'application/json'
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAdminUser]       
+@api_view(['POST'])
+@permission_classes([])
+def new(request):
+    token = request.headers.get('Authorization', None)
+    body = request.data
+    email = body.get('email', None)
+    if not token:
+        return Response('error, expected token', status=status.HTTP_400_BAD_REQUEST)
+    
+    if not email_is_valid(email):
+        return Response('error, expected email or is invalid', status=status.HTTP_400_BAD_REQUEST)
+    
+    magic = magiclink.MagicLinkAuth()
+    res, err = magic.didtoken_is_valid(token, email)
+    if not res:
+        return Response(err, status=status.HTTP_400_BAD_REQUEST)
+    user, err = create_user(body)
+    if err:
+        return Response(err, status=status.HTTP_400_BAD_REQUEST)
+    user_serialized = UserSerializer(user).data
+    return Response(user_serialized, status=status.HTTP_201_CREATED)
 
-    @action(detail=False, methods=['POST'], permission_classes=[])
-    def new(self, request):
-        token = request.headers.get('Authorization', None)
-        body = request.data
-        email = body.get('email', None)
-        if not token:
-            return Response('error, expected token', status=status.HTTP_400_BAD_REQUEST)
-        
-        if not email_is_valid(email):
-            return Response('error, expected email or is invalid', status=status.HTTP_400_BAD_REQUEST)
-        
-        magic = magiclink.MagicLinkAuth()
-        res, err = magic.didtoken_is_valid(token, email)
-        if not res:
-            return Response(err, status=status.HTTP_400_BAD_REQUEST)
-        user, err = create_user(body)
-        if err:
-            return Response(err, status=status.HTTP_400_BAD_REQUEST)
-        user_serialized = UserSerializer(user).data
-        return Response(user_serialized, status=status.HTTP_201_CREATED)
 
-@require_http_methods(['GET'])
+@api_view(['GET'])
+@permission_classes([])
 def get_user(request, email):
     if not email_is_valid(email):
-        error_msj = json.dumps({'description': 'email invalid'})
-        return HttpResponseBadRequest(error_msj, content_type=APPLICATION_JSON)
+        return Response({'description': 'email invalid'}, status=status.HTTP_400_BAD_REQUEST)
     users = User.objects.filter(email=email)
     if not users.exists():
-        error_msj = json.dumps({'description': 'user does not exists'})
-        return HttpResponseNotFound(error_msj, content_type=APPLICATION_JSON)
-    return HttpResponse('ok')
+        return Response({'description': 'email not registered'}, status=status.HTTP_404_NOT_FOUND)
+    return Response({'description': 'ok'})
 
 def email_is_valid(email):
     import re 
