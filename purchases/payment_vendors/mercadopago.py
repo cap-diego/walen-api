197e1f62db84a3ff2727371ab5364db518bb0978
Diego <diegobuceta35@gmail.com>
2021-03-02 12:57:35 -0300

    refactor: MP service

--- purchases/payment_vendors/mercadopago.py
+++ purchases/payment_vendors/mercadopago.py
@@ -8,6 +8,10 @@ import requests
 from requests.exceptions import Timeout
 from requests.exceptions import ConnectionError
 
+FAILED_STATUS = 'rejected'
+RESERVED_STATUS = 'authorized'
+CAPTURED_STATUS = 'captured'
+
 class MercadoPagoPaymentService:
     headers = { 'Authorization': 'Bearer {}'
                 .format(settings.MERCADO_PAGO_ACCESS_TOKEN)
@@ -27,20 +31,14 @@ class MercadoPagoPaymentService:
                     headers=cls.headers, \
                     timeout=settings.MERCADO_PAGO_TIMEOUT)
         except Timeout:
-            error_msg = 'error timeout'
+            return 'error timeout', False
         except ConnectionError as err:
-            error_msg = '{}'.format(err)
-        if response.status_code != 201:
-            error_msg = '{} [{}]'.format(response.json().get('message',''), \
-                    response.json().get('error',''))
-        if error_msg:
-            return error_msg, False
-
-        return response.json(), True
-    
+            return '{}'.format(err), False
+        
+        return cls.parse_response(response=response, expected_status=RESERVED_STATUS)
+        
     @classmethod
     def capture(cls, purch_id):
-        error_msg = ''
         data = {
             'capture': True
         }
@@ -53,14 +51,21 @@ class MercadoPagoPaymentService:
                     headers=cls.headers, \
                     timeout=settings.MERCADO_PAGO_TIMEOUT)
         except Timeout:
-            error_msg = 'error timeout'
+            return 'error timeout', False
         except ConnectionError as err:
-            error_msg = '{}'.format(err)
+            return '{}'.format(err), False
         
-        if response.status_code not in [200, 201]:
+        return cls.parse_response(response=response, expected_status=CAPTURED_STATUS)
+
+
+    @classmethod
+    def parse_response(cls, response, expected_status):
+        if response.status_code != 201: 
             error_msg = '{}'.format(response.json().get('message', ''))
-        
-        if error_msg:
             return error_msg, False
-        
-        return response.json(), True
+        else:
+            if response.json()['status'] == expected_status:
+                return expected_status, True
+            error_msg = '{} {}'.format(response.json()['status'], response.json()['status_detail'])
+            return error_msg, False
+                
