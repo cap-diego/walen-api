844e0cc1500434e96bd3bf82d8f26a6e6f633e53
Diego <diegobuceta35@gmail.com>
2021-03-03 19:00:42 -0300

    agrego comando para cancelar pagos de compras no completadas y vencidas

---
+++ purchases/management/commands/return_payment_expired_non_complete_purchase.py
@@ -0,0 +1,45 @@
+# From django
+from django.core.management.base import BaseCommand, CommandError
+
+# From w
+from purchases.models import Payment
+from purchases.constants import PURCHASE_STATUS_CANCELLED,  \
+    PAYMENT_STATUS_RESERVED    
+from purchases.signals import cancel_payment 
+from ecommerceapp.time import APIClock
+
+class Command(BaseCommand):
+    help = 'Returns the payments of expired and non completed Purchases'
+
+    def handle(self, *args, **options):
+        now = APIClock.now()
+        payments_reserved_purchase_non_cancelled = Payment.objects.exclude(
+            individual_purchase__purchase__status=PURCHASE_STATUS_CANCELLED
+        ).filter(
+            status=PAYMENT_STATUS_RESERVED
+        )
+        
+        total_to_update = 0
+        total_updated = 0
+
+        for payment_reserved in payments_reserved_purchase_non_cancelled:
+            
+            purchase = payment_reserved.individual_purchase.purchase
+            if APIClock.date_is_expired(purchase.expiration_date):
+                continue
+
+            total_to_update +=1
+            res, ok = cancel_payment(payment_reserved)
+
+            if ok:
+                total_updated += 1
+            else:
+                self.stdout.write(self.style.ERROR(
+                    '{} {} {}'.format(res, ok,  payment_reserved.id)
+                ))
+        
+        self.stdout.write(self.style.SUCCESS(
+            'Resumen de corrida. Total para cancelar: {}, total cancelados: {}'.format(
+                total_to_update, total_updated
+            )
+        ))
