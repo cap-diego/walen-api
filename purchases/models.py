d67415bd209778357858054496b14432cc2aff94
Diego <diegobuceta35@gmail.com>
2021-03-03 00:13:38 -0300

    cambio tipo de ID para los cupones

--- purchases/models.py
+++ purchases/models.py
@@ -7,6 +7,7 @@ from django.core.validators import MinValueValidator, \
 # Utils 
 import uuid
 from datetime import timedelta
+import string, random
 
 # From w
 from purchases.constants import PURCHASE_STATUS_CHOICES, \
@@ -336,10 +337,10 @@ def get_or_create_addr(addr):
 
 class Coupon(models.Model):
 
-    id = models.UUIDField(primary_key=True, \
-        default=uuid.uuid4, editable=False, \
-        verbose_name='Coupon id' )
-
+    id = models.CharField(primary_key=True,
+            max_length=6,
+            verbose_name='Coupon id')
+    
     discount_percent = models.PositiveSmallIntegerField(
         validators=[MinValueValidator(0, 'error, el porcentaje no puede ser inferior a 0'), \
             MaxValueValidator(99, 'error, el porcentaje no puede superar 99')]
@@ -348,7 +349,22 @@ class Coupon(models.Model):
     valid_until = models.DateField()
 
     def __str__(self):
-        return 'Cupon de {} de descuento'.format(self.discount_percent)
+        return '[{}] Cupon de {} de descuento'.format(self.id, self.discount_percent)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            while (True):
+                new_id = generate_random(6)
+                if not Coupon.objects.filter(id=new_id).exists():
+                    break
+            self.id = new_id
+        self.id = self.id.upper()
+        super().save(*args, **kwargs)
+        
 
     class Meta:
         ordering = ['-valid_until']
+
+
+def generate_random(n):
+    return ''.join(random.sample(string.ascii_uppercase, n))
